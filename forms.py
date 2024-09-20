77d33695efe13a785a26863aac2f0ced4d377549
Fabioomega <fabioms2002@gmail.com>
2024-09-20 00:05:26 -0300

    Added the login style and the sass version of the login

--- forms.py
+++ forms.py
@@ -2,9 +2,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from flask_wtf import FlaskForm
 from wtforms.validators import InputRequired
 
+
 class LoginForm(FlaskForm):
-    username = StringField('Usuário', validators=[InputRequired()],
-                           render_kw={"placeholder": "Usuário"})
-    password = PasswordField('Senha', validators=[InputRequired()],
-                             render_kw={"placeholder": "Senha"})
-    submit = SubmitField('Login')
+    username = StringField("Usuário", validators=[InputRequired()])
+    password = PasswordField("Senha", validators=[InputRequired()])
+    submit = SubmitField("Login")
