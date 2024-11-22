53fdd50807743d159b11dfc99b2d819e320a2ca8
Fabioomega <fabioms2002@gmail.com>
2024-11-22 12:31:15 -0300

    Added a beta for the form

--- static/src/application/form.tsx
+++ static/src/application/form.tsx
@@ -1,58 +1,392 @@
 import "../styles/form.scss";
-const React = require('react');
-const { createRoot } = require('react-dom/client');
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Header } from "../components/Header.tsx";
+import { Processor, DefaultFunctions } from "../js/processor.ts";
 
-let test = [
-    {
-        "id": 0,
-        "texto": "Escreva seu endereço",
-        "tipo": "Textual",
-        "variavel_associacao": "endereco"
-    },
-    {
-        "id": 1,
-        "texto": "Qual das opções é melhor?",
-        "tipo": "Multipla Escolha",
-        "variavel_associacao": "endereco"
-    }
-];
+interface ProcessorFunctions {
+	addVariable: (varName: string, value: any) => void,
+}
+
+enum QuestionType {
+	SingleCategoricalQuestion = 'objetva',
+}
+
+interface Option {
+	id: number,
+	ordem: number,
+	pontuacao: number,
+	texto: string
+}
+
+interface ObjectiveQuestion {
+	id: number,
+	varName: string,
+	text: string,
+	necessary: boolean,
+	options: Array<Option>,
+}
+
+interface FormulaQuestion {
+	text: string,
+	formula: string,
+	process: Processor
+}
+
+interface PerguntaCategorica {
+	id: number,
+	id_formulario: number,
+	nome_variavel: string,
+	obrigatoria: boolean,
+	opcoes: Array<Option>,
+	ordem: number,
+	tabela_conversao: Object,
+	texto: string,
+	tipo: QuestionType
+}
+
+interface RenderJSON {
+	id_formulario: number,
+	pagina_atual: number,
+	total_paginas: number,
+	total_registros: number,
+	perguntas: Array<PerguntaCategorica>
+}
+
+let test = {
+	"perguntas": [
+		{
+			"id": 4,
+			"id_formulario": 1,
+			"nome_variavel": "forca",
+			"obrigatoria": false,
+			"opcoes": [
+				{
+					"id": 7,
+					"ordem": 1,
+					"pontuacao": 0,
+					"texto": "Nenhuma"
+				},
+				{
+					"id": 8,
+					"ordem": 2,
+					"pontuacao": 1,
+					"texto": "Alguma"
+				},
+				{
+					"id": 9,
+					"ordem": 3,
+					"pontuacao": 2,
+					"texto": "Muita ou não consegue"
+				}
+			],
+			"ordem": 1,
+			"tabela_conversao": {
+				"Alguma": 1,
+				"Muita ou não consegue": 2,
+				"Nenhuma": 0
+			},
+			"texto": "O quanto de dificuldade você tem para levantar e carregar 5kg?",
+			"tipo": "objetiva"
+		},
+		{
+			"id": 5,
+			"id_formulario": 1,
+			"nome_variavel": "ajuda_caminhar",
+			"obrigatoria": false,
+			"opcoes": [
+				{
+					"id": 10,
+					"ordem": 1,
+					"pontuacao": 0,
+					"texto": "Nenhuma"
+				},
+				{
+					"id": 11,
+					"ordem": 2,
+					"pontuacao": 1,
+					"texto": "Alguma"
+				},
+				{
+					"id": 12,
+					"ordem": 3,
+					"pontuacao": 2,
+					"texto": "Muita, usa apoios ou é incapaz"
+				}
+			],
+			"ordem": 2,
+			"tabela_conversao": {
+				"Alguma": 1,
+				"Muita, usa apoios ou é incapaz": 2,
+				"Nenhuma": 0
+			},
+			"texto": "O quanto de dificuldade você tem para atravessar um cômodo?",
+			"tipo": "objetiva"
+		},
+		{
+			"id": 6,
+			"id_formulario": 1,
+			"nome_variavel": "levantar_cadeira",
+			"obrigatoria": false,
+			"opcoes": [
+				{
+					"id": 13,
+					"ordem": 1,
+					"pontuacao": 0,
+					"texto": "Nenhuma"
+				},
+				{
+					"id": 14,
+					"ordem": 2,
+					"pontuacao": 1,
+					"texto": "Alguma"
+				},
+				{
+					"id": 15,
+					"ordem": 3,
+					"pontuacao": 2,
+					"texto": "Muita ou não consegue sem ajuda"
+				}
+			],
+			"ordem": 3,
+			"tabela_conversao": {
+				"Alguma": 1,
+				"Muita ou não consegue sem ajuda": 2,
+				"Nenhuma": 0
+			},
+			"texto": "O quanto de dificuldade você tem para levantar de uma cama ou cadeira?",
+			"tipo": "objetiva"
+		},
+		{
+			"id": 7,
+			"id_formulario": 1,
+			"nome_variavel": "subir_escadas",
+			"obrigatoria": false,
+			"opcoes": [
+				{
+					"id": 16,
+					"ordem": 1,
+					"pontuacao": 0,
+					"texto": "Nenhuma"
+				},
+				{
+					"id": 17,
+					"ordem": 2,
+					"pontuacao": 1,
+					"texto": "Alguma"
+				},
+				{
+					"id": 18,
+					"ordem": 3,
+					"pontuacao": 2,
+					"texto": "Muita ou não consegue"
+				}
+			],
+			"ordem": 4,
+			"tabela_conversao": {
+				"Alguma": 1,
+				"Muita ou não consegue": 2,
+				"Nenhuma": 0
+			},
+			"texto": "O quanto de dificuldade você tem para subir um lance de escadas de 10 degraus?",
+			"tipo": "objetiva"
+		},
+		{
+			"id": 8,
+			"id_formulario": 1,
+			"nome_variavel": "quedas",
+			"obrigatoria": false,
+			"opcoes": [
+				{
+					"id": 19,
+					"ordem": 1,
+					"pontuacao": 0,
+					"texto": "Nenhuma"
+				},
+				{
+					"id": 20,
+					"ordem": 2,
+					"pontuacao": 1,
+					"texto": "1-3 quedas"
+				},
+				{
+					"id": 21,
+					"ordem": 3,
+					"pontuacao": 2,
+					"texto": "4 ou mais quedas"
+				}
+			],
+			"ordem": 5,
+			"tabela_conversao": {
+				"1-3 quedas": 1,
+				"4 ou mais quedas": 2,
+				"Nenhuma": 0
+			},
+			"texto": "Quantas vezes você caiu no último ano?",
+			"tipo": "objetiva"
+		},
+		{
+			"id": 9,
+			"id_formulario": 1,
+			"nome_variavel": "cp",
+			"obrigatoria": false,
+			"opcoes": [
+				{
+					"id": 22,
+					"ordem": 1,
+					"pontuacao": 0,
+					"texto": " Mulher e CP > 33cm"
+				},
+				{
+					"id": 23,
+					"ordem": 2,
+					"pontuacao": 10,
+					"texto": "Mulher e CP <= 33cm"
+				},
+				{
+					"id": 24,
+					"ordem": 3,
+					"pontuacao": 0,
+					"texto": "Homens e CP > 34cm"
+				},
+				{
+					"id": 25,
+					"ordem": 4,
+					"pontuacao": 10,
+					"texto": "Homens e CP <= 34cm"
+				}
+			],
+			"ordem": 6,
+			"tabela_conversao": {
+				" Mulher e CP > 33cm": 0,
+				"Homens e CP <= 34cm": 10,
+				"Homens e CP > 34cm": 0,
+				"Mulher e CP <= 33cm": 10
+			},
+			"texto": "Qual valor da Circunferência da Panturrilha?",
+			"tipo": "objetiva"
+		}
+	],
+};
+
+function JSONRender({ json }: { json: RenderJSON }) {
+	let rendered = []
+	json.perguntas.forEach((question) => {
+		switch (question.tipo) {
+			case QuestionType.SingleCategoricalQuestion:
+				rendered.push((
+					
+				));
+				break;
+		}
+	});
+
+	return (
+		<>
+			{rendered}
+		</>
+	);
+}
 
 function TextQuestion({ text }: { text: string }) {
-    return (
-        <div className="card">
-            <p>{text}</p>
-            <input></input>
-        </div>
-    );
+	return (
+		<div className="card">
+			<p>{text}</p>
+			<input className="writable-field"></input>
+		</div>
+	);
 }
 
 function NumericQuestion({ text }: { text: string }) {
-    return (
-        <div className="card">
-            <p>{text}</p>
-            <input type="number"></input>
-        </div>
-    );
+	return (
+		<div className="card">
+			<p>{text}</p>
+			<input className="writable-field" type="number"></input>
+		</div>
+	);
 }
 
-function Formula({ text }: { text: string }) {
-    return (
-        <div className="card force-shorter">
-            <p>{text} Output Aqui</p>
-        </div>
-    );
+function SingleCategoricalQuestion({ id, varName, text, necessary, options, addVariable }: ObjectiveQuestion) {
+
+	let optionsEl: Array<Element> = [];
+	let [idChecked, setIdChecked] = useState(id);
+
+	options.forEach((op, i) => {
+		optionsEl.push(
+			(
+				<div key={i}>
+					<input id={varName + (id + i).toString()} type="radio" onChange={() => { setIdChecked(id + i); addVariable(varName, text) }} checked={idChecked == (id + i)}></input>
+					<label htmlFor={varName + (id + i).toString()}>{op.texto}</label>
+				</div >
+			)
+		);
+	});
+
+	return (
+		<div className="card">
+			<p>{text}</p>
+			{optionsEl}
+		</div>
+	);
+}
+
+function MultipleCategoricalQuestion({ id, text }: { id: number, text: string }) {
+
+	let options: Array<Element> = [];
+	let [idsChecked, setIdsChecked]: [Set<number>, (id: Set<number>) => void] = useState(new Set());
+
+	const invertStateForId = (idToBeInverted: number) => {
+		let temp = new Set(idsChecked);
+
+		if (idsChecked.has(idToBeInverted)) {
+			temp.delete(idToBeInverted);
+			setIdsChecked(temp);
+		} else {
+			temp.add(idToBeInverted);
+			setIdsChecked(temp);
+		}
+	};
+
+	for (let i = 0; i < 3; i++) {
+		options.push(
+			(
+				<div key={i}>
+					<input id={'category' + (id + i).toString()} type="checkbox" onChange={() => { invertStateForId(i + id) }} checked={idsChecked.has(id + i)}></input>
+					<label htmlFor={'category' + (id + i).toString()}>Test</label>
+				</div >
+			)
+		);
+	}
+
+	return (
+		<div className="card">
+			<p>{text}</p>
+			{options}
+		</div>
+	);
+}
+
+function Formula({ text, formula, process }: FormulaQuestion) {
+	return (
+		<div className="card force-shorter">
+			<p>{text}: {process.exec(formula)}</p>
+		</div>
+	);
 }
 
 function Form() {
-    return (<>
-        <Header />
-        <div className="questions-field align-in-column">
-            <TextQuestion text="Nome:"></TextQuestion>
-            <NumericQuestion text="Idade:"></NumericQuestion>
-            <Formula text="Formula:"></Formula>
-        </div>
-    </>);
+	let categoryId = -1;
+	const processor = new Processor();
+
+	const addVarialble = (varName: string, value: any) => {
+		processor.addVariable(varName, value);
+	};
+
+	return (<>
+		<Header />
+		<div className="questions-field align-in-column">
+		</div>
+	</>);
 }
 
 const root = document.getElementById('base-form');
---
+++ static/src/js/processor.ts
@@ -0,0 +1,126 @@
+const seRegex = {
+    'equal': /((?:\W+|^)se\(.+?)\=+(.+?,.+?\))/g,
+    'or': /((?:\W+|^)se\(.+?)ou(.+?,.+?\))/g,
+    'and': /((?:\W+|^)se\(.+?)e(.+?,.+?\))/g,
+    'seReplace': /(\W+|^)se(\(.+?\))/g
+};
+
+const ignoreFunctionsRegex = {
+    'fetch': /(\W+|^)fetch(\(.*?\))/g,
+    'eval': /(\W+|^)eval(\(.*?\))/g,
+    'ignoreFunctionsRegex': /(\W+|^)ignoreFunctionsRegex(\w|[.()])*/g,
+}
+
+const preProcessor = [
+    (formula) => {
+        Object.values(ignoreFunctionsRegex).forEach((reg) => {
+            formula = formula.replace(reg, '');
+        });
+        return formula;
+    },
+    (formula) => {
+        formula = formula.replace(seRegex.equal, "$1==$2").replace(seRegex.or, "$1||$2").replace(seRegex.and, "$1&&$2")
+            .replace(seRegex.seReplace, "$1DefaultFunctions.se$2");
+        return formula;
+    }
+];
+
+export class DefaultFunctions {
+    static se(condition, on_true, on_false) {
+        if (condition) {
+            return on_true;
+        } else if (on_false != undefined) {
+            return on_false;
+        }
+
+        return 0;
+    }
+}
+
+export class Processor {
+    variables: Object;
+    conversion_table: Object;
+
+    static #preProcess(formula) {
+        preProcessor.forEach((func) => {
+            formula = func(formula);
+        });
+
+        return formula;
+    }
+
+    static #replaceVariables(variables, formula) {
+        let out = formula;
+        variables.forEach((variable) => {
+            out = out.replaceAll(variable, `world_obj.${variable}`);
+        }
+        );
+        return out;
+    }
+
+    #convertValueToString(variable, value) {
+        if (variable in this.conversion_table) {
+            return this.conversion_table[variable][value];
+        }
+
+        let output = parseInt(value);
+        if (isNaN(output)) {
+            return 0;
+        }
+
+        return output;
+    }
+
+    constructor() {
+        this.variables = {};
+        this.conversion_table = {};
+    }
+
+    addVariable(variable, value) {
+        if (typeof value === 'string') {
+            this.variables[variable] = this.#convertValueToString(variable, value);
+        } else if (typeof value === 'number') {
+            this.variables[variable] = value;
+        } else {
+            console.warn(`Unexpected variable ${typeof value} passed! It must be either 'string' or 'number'`);
+            this.variables[variable] = 0;
+        }
+    }
+
+    clearVariables() {
+        this.variables = {};
+    }
+
+
+    addTable(table) {
+        Object.assign(this.conversion_table, table);
+    }
+
+    clearTables() {
+        this.conversion_table = {};
+    }
+
+    reset() {
+        this.clearVariables();
+        this.clearTables();
+    }
+
+    exec(formula) {
+        formula = formula.replaceAll(' ', '');
+        const variables_name = Object.keys(this.variables);
+
+        if (variables_name.length == 0 || !formula) {
+            return 0;
+        }
+
+        formula = Processor.#preProcess(formula);
+        console.log(formula);
+
+        try {
+            const world_obj = this.variables;
+            return eval(Processor.#replaceVariables(variables_name, formula));
+        } catch {
+            return 0;
+        }
+    }
+}
