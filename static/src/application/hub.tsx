779b5e9134295d99920fc7f81da818d5f87a6582
Fabioomega <fabioms2002@gmail.com>
2024-11-27 23:01:45 -0300

    Fixed the Header and added return options

--- static/src/application/hub.tsx
+++ static/src/application/hub.tsx
@@ -8,7 +8,7 @@ import { Header } from "../components/Header.tsx";
 function Hub() {
     return (
         <>
-            <Header title='Bem Vindo “Usuario”' logout={true} />
+            <Header title='Bem Vindo “Usuario”' logout={true} return_link={window.url_to_logout} />
             <nav>
                 <IconButton className="no-style hub-icon-button" iconPath={"/static/icons/torso.svg"}>Adicionar Usuario</IconButton>
                 <IconButton className="no-style hub-icon-button" iconPath={"/static/icons/plus.svg"}>Adicionar Projeto</IconButton>
--- static/src/components/Header.tsx
+++ static/src/components/Header.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 
 interface headerProp {
     title?: string,
+    return_link: string,
     logout: boolean
 }
 
-export const Header: React.FC<headerProp> = ({ title, logout = false }) => {
+export const Header: React.FC<headerProp> = ({ title, return_link, logout = false }) => {
     return (
         <header>
             <button className="no-style logout-button">
-                {logout ? <><span>&#10005;</span>Logout</> : <>Return</>}
+                <a className="no-style" href={return_link}>
+                    {logout ? <><span>&#10005;</span>Logout</> : <>Return</>}
+                </a>
             </button>
             <div className="align-in-row-left">
                 <img className="logo" src="/static/icons/backbone.svg" />
